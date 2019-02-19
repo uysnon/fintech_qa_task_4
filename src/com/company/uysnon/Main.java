d99ca46c09c7dc1e4b89566eda93ac3a26226c42
uysnon <avgorkin1@gmail.com>
2019-02-20 00:58:26 +0300

    first commit

--- src/com/company/uysnon/Main.java
+++ src/com/company/uysnon/Main.java
@@ -1,15 +1,30 @@
 package com.company.uysnon;
 
+import java.util.Scanner;
+
 public class Main {
 
-    private  final String [] strings =
-            {"1. -en Hello World",
-                    "2. -ru Привет, мир",
-                    "3. -CHN 你好，世界"
-                    "4.  مرحبا بالعالم"
-    } ;
+    private static final String [][] STRINGS_HELLO_WORLD =
+            {
+                    {"1. -en", "Hello World"},
+                    {"2. -ru", "Привет, мир"},
+                    {"3. -CHN", "你好，世界"},
+                    {"4. -ar", "مرحبا بالعالم"},
+                    {"5. -de", "Hallo Welt"},
+                    {"6. -fr", "Bonjour le monde"}
+    };
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println("Write number of item in the following list:");
+        for (int i = 0; i< STRINGS_HELLO_WORLD.length; i++) {
+            System.out.println(STRINGS_HELLO_WORLD[i][0]);
+        }
+        int number = -1;
+        Scanner scanner = new Scanner(System.in); //Сканнер для считывания номера, по которому выведится стркоа
+        while ((number<1)||(number>STRINGS_HELLO_WORLD.length)){
+            number = scanner.nextInt();
+        }
+        scanner.close();
+        System.out.println(STRINGS_HELLO_WORLD[number-1][1]);
     }
 }
