bfe722b95ef742f80bea8ce1bc0b1ebcf28349b9
uysnon <avgorkin1@gmail.com>
2019-02-19 12:40:33 +0300

    test commit 1

---
+++ src/com/company/uysnon/Main.java
@@ -0,0 +1,15 @@
+package com.company.uysnon;
+
+public class Main {
+
+    private  final String [] strings =
+            {"1. -en Hello World",
+                    "2. -ru Привет, мир",
+                    "3. -CHN 你好，世界"
+                    "4.  مرحبا بالعالم"
+    } ;
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
